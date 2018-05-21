df69e13f10ce6daa7dd43269178ca875d121a478
feitian124 <feitian124@qq.com>
2018-05-22 00:37:06 +0800

    enhance for @EmbeddedId

--- src/main/java/com/yunnuy/ffdc/controller/ChangeController.java
+++ src/main/java/com/yunnuy/ffdc/controller/ChangeController.java
@@ -1,13 +1,10 @@
 package com.yunnuy.ffdc.controller;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.google.common.collect.Lists;
 import com.yunnuy.ffdc.common.R;
 import com.yunnuy.ffdc.model.Change;
 import com.yunnuy.ffdc.service.ChangeService;
@@ -22,9 +19,8 @@ public class ChangeController {
 	@GetMapping("index")
 	public R index() {
 		Iterable<Change> data = changeService.findAll();
-		List<Change> list = Lists.newArrayList(data);
 		R r = R.ok();
-		r.put("list", list);
+		r.put("data", data);
 		return r;
 	}
 
--- src/main/java/com/yunnuy/ffdc/model/ChangePrimarykey.java
+++ src/main/java/com/yunnuy/ffdc/model/ChangePrimarykey.java
@@ -39,4 +39,38 @@ public class ChangePrimarykey implements Serializable {
 	public void setChangeDate(Date changeDate) {
 		this.changeDate = changeDate;
 	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((changeDate == null) ? 0 : changeDate.hashCode());
+		result = prime * result + ((fullname == null) ? 0 : fullname.hashCode());
+		result = prime * result + id;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		ChangePrimarykey other = (ChangePrimarykey) obj;
+		if (changeDate == null) {
+			if (other.changeDate != null)
+				return false;
+		} else if (!changeDate.equals(other.changeDate))
+			return false;
+		if (fullname == null) {
+			if (other.fullname != null)
+				return false;
+		} else if (!fullname.equals(other.fullname))
+			return false;
+		if (id != other.id)
+			return false;
+		return true;
+	}
 }
